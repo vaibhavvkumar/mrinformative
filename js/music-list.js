150a91f64404739f1bf9c9546e753fe9f7dd277c
vaibhavvkumar <91684653+vaibhavvkumar@users.noreply.github.com>
2021-10-16 12:33:50 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,195 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Activate Chackras",
+    artist: "Vaibhav Kumar",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: " Astral Travel",
+    artist: "Vaibhav Kumar",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Brain Booster",
+    artist: "Vaibhav Kumar",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Brain Massage",
+    artist: "Vaibhav Kumar",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Buddha",
+    artist: "Vaibhav Kumar",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Concentration",
+    artist: "Vaibhav Kumar",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Ekagraman",
+    artist: "Vaibhav Kumar",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Euphoria",
+    artist: "Vaibhav Kumar",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Feel Good",
+    artist: "Vaibhav Kumar",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Healing",
+    artist: "Vaibhav Kumar",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Meditation",
+    artist: "Vaibhav Kumar",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Mindfulness",
+    artist: "Vaibhav Kumar",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Mood Fresh",
+    artist: "Vaibhav Kumar",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "Motivation",
+    artist: "Vaibhav Kumar",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "Mantras",
+    artist: "Vaibhav Kumar",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "Nataraja",
+    artist: "Vaibhav Kumar",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "Negative Energy",
+    artist: "Vaibhav Kumar",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "OM 432Hz",
+    artist: "Vaibhav Kumar",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "Powerful Thoughts",
+    artist: "Vaibhav Kumar",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "Rain Sounds",
+    artist: "Vaibhav Kumar",
+    img: "music-20",
+    src: "music-20"
+  },
+  {
+    name: "Relax",
+    artist: "Vaibhav Kumar",
+    img: "music-21",
+    src: "music-21"
+  },
+  {
+    name: "Samadhi",
+    artist: "Vaibhav Kumar",
+    img: "music-22",
+    src: "music-22"
+  },
+  {
+    name: "Deep Sleep",
+    artist: "Vaibhav Kumar",
+    img: "music-23",
+    src: "music-23"
+  },
+  {
+    name: "Third Eye",
+    artist: "Vaibhav Kumar",
+    img: "music-24",
+    src: "music-24"
+  },
+  {
+    name: "Vipassana",
+    artist: "Vaibhav Kumar",
+    img: "music-25",
+    src: "music-25"
+  },
+  {
+    name: "Visualize",
+    artist: "Vaibhav Kumar",
+    img: "music-26",
+    src: "music-26"
+  },
+  {
+    name: "Yog Nidra",
+    artist: "Vaibhav Kumar",
+    img: "music-27",
+    src: "music-27"
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
